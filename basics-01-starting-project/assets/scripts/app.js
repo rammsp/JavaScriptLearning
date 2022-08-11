d19b29269e696a33968bbb901914a59831579c9d
rammsp <stulasappa@tracfone.com>
2022-08-11 08:10:22 +0400

    11/08/2022

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,43 +1,41 @@
 // alert('This Works!!!');
 
 function add(num1, num2) {
-    const result = num1 + num2;
-    alert('Reuslt is ' + result);
+  const result = num1 + num2;
+  alert("Reuslt is " + result);
 }
 
 add(1, 2);
 add(3, 4);
 
-let displayResult
-function addition() {
-
-}
+let displayResult;
+function addition() {}
 
 let currentResult;
 currentResult = 0;
 const initialValue = 0;
 currentResult = currentResult + 1 + 10 * 3;
 // let calculationDescription = currentResult + ' = ' + initialValue + ' + 1 + 10 * 3';
-let calculationDescription = `${currentResult}` + ' = ' + initialValue + '\n + 1 + 10 * 3';
+let calculationDescription =
+  `${currentResult}` + " = " + initialValue + "\n + 1 + 10 * 3";
 //Uses method from vendor.js
 outputResult(currentResult, calculationDescription);
 
 let logEntries = [];
 
-
 let finalResult = 100;
 function addition() {
-    let initialValue = parseInt(finalResult);
-    finalResult =  initialValue + parseInt(userInput.value);
-    let logEntry = {
-        action: 'ADDITION',
-        initValue: initialValue,
-        input: userInput.value,
-        finResult: finalResult
-    }
-    logEntries.push(logEntry);
-    console.log(logEntries);
-    outputResult(finalResult, `100 + ${userInput.value}`);
+  let initialValue = parseInt(finalResult);
+  finalResult = initialValue + parseInt(userInput.value);
+  let logEntry = {
+    action: "ADDITION",
+    initValue: initialValue,
+    input: userInput.value,
+    finResult: finalResult,
+  };
+  logEntries.push(logEntry);
+  console.log(logEntries);
+  outputResult(finalResult, `100 + ${userInput.value}`);
 }
 
-addBtn.addEventListener('click', addition);
+addBtn.addEventListener("click", addition);
